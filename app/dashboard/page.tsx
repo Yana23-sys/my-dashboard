13e5de22f63a54da8d5dd2089b990e74fb0b352e
Yana Turiak <turyak1998@gmail.com>
2024-10-07 18:44:43 +0100

    create ProductActions component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import FilterDropdown from "@/app/ui/dashboard/filter";
+import ProductActions from "@/app/ui/dashboard/actions";
 import ProductsList from "@/app/ui/dashboard/list";
 import StatusSelect from "@/app/ui/dashboard/status";
 
@@ -20,29 +20,8 @@ export default function Dashboard() {
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
 
         <Tabs defaultValue="all">
-          <div className="flex items-center">
-
-            <StatusSelect />
-
-            <div className="ml-auto flex items-center gap-2">
-              
-              <FilterDropdown />
-
-              <Button size="sm" variant="outline" className="h-7 gap-1">
-                <File className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                  Export
-                </span>
-              </Button>
-
-              <Button size="sm" className="h-7 gap-1">
-                <PlusCircle className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                  Add Product
-                </span>
-              </Button>
-            </div>
-          </div>
+          
+          <ProductActions />
 
           <TabsContent value="all">
 
